133cf2b1975b47410dad974ca1d58efcd4b4a65f
gurbur <gurbur331@ajou.ac.kr>
2025-03-18 18:54:47 +0900

    Refact: divide types into one file

--- parser.c
+++ parser.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include "list_head.h"
+#include "types.h"
 
 /*
  * TODO:
@@ -16,45 +18,6 @@
  * 5. [ ] if given string has inline element, tokenize it
  * 6. [ ] print the output into specific file format
 */
-
-typedef enum {
-	NONE_BLOCK = 0,
-	HEADING1,
-	HEADING2,
-	HEADING3,
-	CODE_BLOCK,
-	LINE,
-	ORDERED_LIST,
-	UNORDERED_LIST,
-	PARAGRAPH,
-	IMAGE_LINK,
-} block_element_type;
-
-typedef enum {
-	NONE_INLINE = 0,
-	NORMAL_STRING,
-	ITALIC,
-	BOLD,
-	ITALIC_AND_BOLD,
-	INLINE_CODE,
-	LINK,
-	LINK_SHOWN,
-	LINK_HIDDEN,
-} inline_element_type;
-
-typedef struct {
-	block_element_type type;
-	char* value;
-	char* code_type;
-	struct inline_element* in_el;
-} block_element;
-
-typedef struct {
-	inline_element_type type;
-	char* value;
-} inline_element;
-
-
 
 
 int main(int argc, char *argv[]) {
--- tokenizer.c
+++ tokenizer.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include "types.h"
 
 /*
  * TODO:
@@ -18,45 +19,6 @@
  * 5. [x] if given string has inline element, tokenize it
  * 6. [x] print the output into specific file format
 */
-
-typedef enum {
-	NONE_BLOCK = 0,
-	HEADING1,
-	HEADING2,
-	HEADING3,
-	CODE_BLOCK,
-	LINE,
-	ORDERED_LIST,
-	UNORDERED_LIST,
-	PARAGRAPH,
-	IMAGE_LINK,
-} block_element_type;
-
-typedef enum {
-	NONE_INLINE = 0,
-	NORMAL_STRING,
-	ITALIC,
-	BOLD,
-	ITALIC_AND_BOLD,
-	INLINE_CODE,
-	LINK,
-	LINK_SHOWN,
-	LINK_HIDDEN,
-} inline_element_type;
-
-typedef struct {
-	block_element_type type;
-	char* value;
-	char* code_type;
-	struct inline_element* in_el;
-} block_element;
-
-typedef struct {
-	inline_element_type type;
-	char* value;
-} inline_element;
-
-// ******************************
 
 block_element_type block_state = NONE_BLOCK;
 
